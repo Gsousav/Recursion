0ec34c32aa764692fc0cf70de543c4469dea7943
Gabriel Sousa <gssousav@gmail.com>
2023-10-10 22:05:02 +0200

    First commit

---
+++ problems/06-add-to-twelve.js
@@ -0,0 +1,30 @@
+/***********************************************************************
+Write a recursive function called `addToTwelve` that will return true if there
+are two adjacent numbers in the input array that can sum up to 12. Otherwise,
+return false.
+
+Examples:
+
+addToTwelve([1, 3, 4, 7, 5]); // true
+addToTwelve([1, 3, 4, 7, 6]); // false
+addToTwelve([1, 11, 4, 7, 6]); // true
+addToTwelve([1, 12, 4, 7, 6]); // false
+addToTwelve([1]); // false
+***********************************************************************/
+
+const addToTwelve = numbers => {
+  if (numbers.length < 2) {
+    return false;
+  } else if ((numbers[0] + numbers[1]) === 12) {
+    return true;
+  } else {
+    return addToTwelve(numbers.slice(1));
+  }
+}
+
+/**************DO NOT MODIFY ANYTHING UNDER THIS LINE*****************/
+try {
+  module.exports = addToTwelve;
+} catch (e) {
+  module.exports = null;
+}
---
+++ problems/17-bonus-permutations.js
@@ -0,0 +1,41 @@
+/***********************************************************************
+Write a recursive method permutations(array) that calculates all the
+permutations of the given array. For an array of length n there are n! different
+permutations. So for an array with three elements we will have 3 * 2 * 1 = 6
+different permutations.
+
+Examples:
+
+permutations([1, 2]) // [[1, 2], [2, 1]]
+permutations([1, 2, 3]) // [[1, 2, 3], [1, 3, 2],
+                        // [2, 1, 3], [2, 3, 1],
+                        // [3, 1, 2], [3, 2, 1]]
+***********************************************************************/
+
+const permutations = array => {
+  if (array.length === 0) {
+    return [[]];
+  }
+
+  const result = [];
+
+  for (let i = 0; i < array.length; i++) {
+    const current = array[i];
+    const remaining = array.slice(0, i).concat(array.slice(i + 1));
+    const subPermutations = permutations(remaining);
+
+    for (const subPermutation of subPermutations) {
+      result.push([current, ...subPermutation]);
+    }
+  }
+
+  return result;
+};
+
+
+/**************DO NOT MODIFY ANYTHING UNDER THIS LINE*****************/
+try {
+  module.exports = permutations;
+} catch (e) {
+  module.exports = null;
+}
